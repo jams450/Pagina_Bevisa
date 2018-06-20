d7311a0c3d52d947bbec45edbb8a1443977ba010
jams450 <jams45072@gmail.com>
2018-06-20 15:36:38 -0500

    Contacto responsivo

--- contacto.php
+++ contacto.php
@@ -12,42 +12,42 @@
     <section id="contact">
       <div class="container">
         <div class="row wow fadeInUp">
-
-
           <div class="col-md-12">
             <div class="contact-about text-center">
               <h3>Bevisa</h3>
               <p>Somos la mejor opción en productos para la industria alimenticia </p>
             </div>
-          </div>
-          <div class="col-md-8">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15054.113099889732!2d-99.0523327!3d19.3895679!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x754ecdc23855aaf3!2sBevisa%2C+S+de+RL+MI!5e0!3m2!1ses-419!2smx!4v1528492431736" width="650" height="280" frameborder="0" style="border:0" allowfullscreen></iframe>
-          </div>
-          <div class="col-md-4">
-            <div class="info">
-              <div>
-                <i class="ion-ios-location-outline"></i>
-                <p>Privada de las Américas Manzana 6 Lote 27 <br>Iztapalapa, CDMX</p>
+            <div class="row">
+              <div class="col-md-8">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15054.113099889732!2d-99.0523327!3d19.3895679!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x754ecdc23855aaf3!2sBevisa%2C+S+de+RL+MI!5e0!3m2!1ses-419!2smx!4v1528492431736" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
               </div>
+              <div class="col-md-4">
+                <div class="info">
+                  <div>
+                    <i class="ion-ios-location-outline"></i>
+                    <p>Privada de las Américas Manzana 6 Lote 27 <br>Iztapalapa, CDMX</p>
+                  </div>
 
-              <div>
-                <i class="ion-ios-email-outline"></i>
-                <p>bevisagaleria@gmail.com</p>
+                  <div>
+                    <i class="ion-ios-email-outline"></i>
+                    <p>bevisagaleria@gmail.com</p>
+                  </div>
+
+                  <div>
+                    <i class="ion-ios-telephone-outline"></i>
+                    <p>59756148</p>
+                  </div>
+
+                  <div>
+                    <i class="fa fa-whatsapp"></i>
+                    <p>5559756148</p>
+                  </div>
+
+                </div>
               </div>
-
-              <div>
-                <i class="ion-ios-telephone-outline"></i>
-                <p>59756148</p>
-              </div>
-
-              <div>
-                <i class="fa fa-whatsapp"></i>
-                <p>5559756148</p>
-              </div>
-
             </div>
-          </div>
 
+          </div>
 
           <div class="col-md-12">
             <div class="contact-about text-center">
